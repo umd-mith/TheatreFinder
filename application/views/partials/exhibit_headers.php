0756d0bc2451bc882e2f720e0a1b864c9903d077
James Smith <jgsmith@gmail.com>
2011-04-12 15:23:44 -0400

    expect Exhibit JSON file to be on same controller as Exhibit view

--- application/views/partials/exhibit_headers.php
+++ application/views/partials/exhibit_headers.php
@@ -1,3 +1,3 @@
-<link href="../theatre_ctrl/exhibit_json" type="application/json" rel="exhibit/data" />
+<link href="exhibit_json" type="application/json" rel="exhibit/data" />
 <script src="http://static.simile.mit.edu/exhibit/api-2.0/exhibit-api.js"
 		type="text/javascript"></script>
