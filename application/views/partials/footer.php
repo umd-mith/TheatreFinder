2f02d66fdaf91e3de4085af93f7fb192d70fb1cc
Dave Lester <davelester@gmail.com>
2011-05-11 16:19:13 -0400

    Cleans up the footer code/navigation.

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,51 +1,11 @@
        <!-- Footer -->
        <div class="grid_12 footer">   
             <ul>
-            	<li class="grid_2 alpha nav_0"><h4><a href="<?php echo base_url();?>main">Home</a></h4>
-					<ul>
-						<li><a href="<?php echo base_url();?>main#about">Introduction</a></li>
-						<li><a href="<?php echo base_url();?>main#updated">Recently updated theatres</a></li>
-                    </ul>
-                </li>
-                <li class="grid_2 nav_1"><h4><a href="<?php echo base_url();?>main/about">About Finder</a></h4>
-                	<ul>
-                    	<li><a href="<?php echo base_url();?>main/about">What finder is</a></li>
-						<li><a href="<?php echo base_url();?>main/about">What finder does</a></li>
-						<li><a href="<?php echo base_url();?>main/about">Standards</a></li>
-						<li><a href="<?php echo base_url();?>main/about">Methodology</a></li>
-                    </ul>
-                </li>
-                <li class="grid_2 nav_2"><h4><a href="<?php echo base_url();?>main/search">Search Finder</a></h4>
-                	<ul>
-                    	<li><a href="<?php echo base_url();?>main/search">Advanced textual search</a></li>
-						<li><a href="<?php echo base_url();?>main/search">Multi-term search</a></li>
-						<li><a href="<?php echo base_url();?>main/search">Search within lists</a></li>
-						<li><a href="<?php echo base_url();?>main/search">Geographic search</a></li>
-                    </ul>
-                </li>
-                <li class="grid_2 nav_3"><h4><a href="<?php echo base_url();?>main/join">Join Finder</a></h4>
-                	<ul>
-                    	<li><a href="<?php echo base_url();?>main/join">Nominate theatres</a></li>
-						<li><a href="<?php echo base_url();?>main/join">Write entries</a></li>
-						<li><a href="<?php echo base_url();?>main/join">Become an editor</a></li>
-						<li><a href="<?php echo base_url();?>main/join">Build a model</a></li>
-						<li><a href="<?php echo base_url();?>main/join">Make suggestions</a></li>
-                    </ul>
-                </li>
-                <li class="grid_2 nav_4"><h4><a href="<?php echo base_url();?>main/contribute">Add to Finder</a></h4>
-                	<ul>
-                    	<li><a href="<?php echo base_url();?>main/contribute">Submit photos or videos</a></li>
-						<li><a href="<?php echo base_url();?>main/contribute">Build a CAMP model</a></li>
-						<li><a href="<?php echo base_url();?>main/contribute">Creative Commons license</a></li>
-                    </ul>
-                </li>
-                <li class="grid_2 omega nav_5"><h4><a href="<?php echo base_url();?>main/resources">Resources</a></h4>
-                	<ul>
-                    	<li><a href="<?php echo base_url();?>main/resources">Master Bibliography</a></li>
-						<li><a href="<?php echo base_url();?>main/resources">Digitized documents</a></li>
-						<li><a href="<?php echo base_url();?>main/resources">Special lists</a></li>
-                    </ul>
-                </li>
+            	<li class="grid_2 alpha nav_0"><h4><a href="<?php echo base_url();?>index">Home</a></h4></li>
+                <li class="grid_2 nav_1"><h4><a href="<?php echo base_url();?>index/about">About Finder</a></h4></li>
+                <li class="grid_2 nav_2"><h4><a href="<?php echo base_url();?>theatres">Theatres</a></h4></li>
+                <li class="grid_2 nav_3"><h4><a href="<?php echo base_url();?>main/join">Contribute</a></h4></li>
+                <li class="grid_2 omega nav_5"><h4><a href="<?php echo base_url();?>main/resources">Resources</a></h4></li>
  			</ul>
        </div> 
 	   	<div class="grid_12 contact">
