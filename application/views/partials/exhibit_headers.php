1264b3a2b9291f09b5b77f8813c4f88dd1cf0a2d
jdickie <jgrantd@gmail.com>
2011-07-19 15:28:59 -0400

    Changing exhibit_headers to include 'text/javascript' type in script type

--- application/views/partials/exhibit_headers.php
+++ application/views/partials/exhibit_headers.php
@@ -3,7 +3,7 @@
 <script src="http://static.simile.mit.edu/exhibit/api-2.0/exhibit-api.js"
 		type="text/javascript"></script>
 <script src="http://static.simile.mit.edu/exhibit/extensions-2.0/time/time-extension.js"></script>
-<script>
+<script type="text/javascript">
 function confirmDeleteTheatre(url) {
   if (confirm("Are you sure you want to delete the theatre?")) {
     document.location = url;
