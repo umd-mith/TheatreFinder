59c581063104fbcdf5ad91a37ba97539bcce45b9
James Smith <jgsmith@gmail.com>
2011-07-19 13:03:20 -0400

    Make database password come from file outside github

--- application/config/database.php
+++ application/config/database.php
@@ -59,10 +59,11 @@ $db['default']['dbcollat'] = "utf8_general_ci";
 /* THEATRES DATABASE stuff 
  * Remember to turn off db_debug in production
  */
-$db['theatrefinder']['hostname'] = "localhost";
+$db['theatrefinder']['hostname'] = "zelda.umd.edu";
 $db['theatrefinder']['username'] = "tf_admin";
-$db['theatrefinder']['password'] = "tfed1t0r";
-$db['theatrefinder']['database'] = "theatrefinder";
+$db['theatrefinder']['password'] = "p455w0rd";
+$db['theatrefinder']['password'] = trim(file_get_contents("/export/software/www/conf/passwords/theatrefinder.db"));
+$db['theatrefinder']['database'] = "ci_theatrefinder";
 $db['theatrefinder']['dbdriver'] = "mysql";
 $db['theatrefinder']['dbprefix'] = "";
 $db['theatrefinder']['pconnect'] = TRUE;
