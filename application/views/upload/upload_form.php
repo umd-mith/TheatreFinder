2b926beb3ce52b326d38359d75ded701beb6f1d0
James Smith <jgsmith@gmail.com>
2011-08-03 11:54:30 -0400

    Increase size limites for images

--- application/views/upload/upload_form.php
+++ application/views/upload/upload_form.php
@@ -4,7 +4,7 @@
 <?php echo form_open_multipart('upload/do_upload/' . $id . '/' . $type);?>
 
 <p>
-	Please select an image to upload showing the <?php echo $type ?> for &ldquo;<?php echo $theatre['theatre_name'] ?>.&rdquo;  Images need to be less than 100k in size and be formatted as .gif, .jpg, or .png.  They also need to be no wider than 1024 pixels and no higher than 768 pixels.
+	Please select an image to upload showing the <?php echo $type ?> for &ldquo;<?php echo $theatre['theatre_name'] ?>.&rdquo;  Images need to be less than 128 megabytes in size and be formatted as .gif, .jpg, or .png.  They also need to be no wider than 4096 pixels and no higher than 3072 pixels.
 </p>
 
 <input type="file" name="userfile" size="20" />
