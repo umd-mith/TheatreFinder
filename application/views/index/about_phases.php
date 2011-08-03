091a374ef889c1eaf449fbcb2b720b704a892d64
James Smith <jgsmith@gmail.com>
2011-08-03 15:30:23 -0400

    Add items from previous production site

---
+++ application/views/index/about_phases.php
@@ -0,0 +1,36 @@
+<!-- Featured theatres -->
+<div class="grid_12 featuredarea">
+    <div class="grid_3 alpha sidebar">
+        <h1>About</h1>
+        <h3></h3>
+                <div id="subnav">
+                <ul>
+                        <li><a href="about_phases">Project Phases</a></li>
+                        <li><a href="about_included">Theatre Types Included</a></li>
+                        <li><a href="about_methodology">Project Methodology</a></li>
+                        <li><a href="about_navigation">Navigating the Site</a></li>
+                </ul>
+                </div>
+    </div>
+    <div class="grid_8 omega maincontent suffix_1">
+        <!-- Content area -->
+        <div class="grid_8 alpha">
+            <h3>The Four Phases of the Theatre Finder Project</h3>
+                                <p>Theatre-finder.org is now in the first phase of its development.</p>
+                                <p>Phase I covers structures from the Minoan theatrical areas of c.2000 BCE to the last theatre built in 1815 C.E.</p>
+                                <p>Phase II will covers structures from 1816-1865</p>
+                                <p>Phase III will cover structures from 1865-1915</p>
+                                <p>Phase IV will update the data annually for theatres reaching their 100th anniversary. It will also
+                                        <ul>
+                                                <li>1.  remove theatres that are lost,</li>
+                                                <li>2.  add theatres that are newly restored to their historic form</li>
+                                                <li>3.  update all entries as needed.</li>
+                                        </ul>
+                                </p>
+                                <p>Through out the project, as additional theatres are discovered, excavated, restored, or, upon further reflection, seem to merit inclusion, they will be added to theatre-finder.org. Theatres that are destroyed, reburied, or, after further consideration, are determined not to be relevant, will be removed.</p>
+                                <p>Research is currently being focused only on identifying theatres built before the end of 1815 and providing full entries (both visitor information and scholarly details sections) for them. The database has been pre-populated with over 550 such theatres, but more remain to be identified, especially in China.</p>
+                                <p>Theatres belonging to Phase II-IV are listed as they turn up while doing other research, but no systematic attempt to identify all of them will be made until additional funding can be secured.</p>
+        </div>
+    </div>
+</div>
+<!-- end featuredarea -->
