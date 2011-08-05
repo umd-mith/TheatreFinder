a62d3589556668e109ac37dca4a51fdab02a68ca
jdickie <jgrantd@gmail.com>
2011-08-05 13:24:10 -0400

    Changing header links

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -7,16 +7,16 @@
                                         <li class="grid_2 nav_1"><h2><a href="<?php echo base_url();?>index">home</a></h2>
                                 <p>Return to the main page<br /></p>
                         </li>
-                        <li class="grid_2 nav_2"><h2><a href="<?php echo base_url();?>index/about">about</a></h2>
+                        <li class="grid_2 nav_2"><h2><a href="<?php echo base_url();?>about">about</a></h2>
                                 <p>information on what is covered in this guide</p>
                         </li>
                         <li class="grid_2 nav_3"><h2><a href="<?php echo base_url();?>theatres">browse</a></h2>
                                 <p>Advanced textual and geographic searches</p>
                         </li>
-                  <li class="grid_2 nav_4"><h2><a href="<?php echo base_url();?>index/contribute">contribute</a></h2>
+                  <li class="grid_2 nav_4"><h2><a href="<?php echo base_url();?>contribute">contribute</a></h2>
                         <p>create &amp; edit entries, and upload photos</p>
                   </li>
-                        <li class="grid_2 omega nav_5"><h2><a href="<?php echo base_url();?>index/resources">resources</a></h2>
+                        <li class="grid_2 omega nav_5"><h2><a href="<?php echo base_url();?>resources">resources</a></h2>
                                 <p>further resources on historic theatres</p>
                         </li>
                                         <div class="clear"></div>
