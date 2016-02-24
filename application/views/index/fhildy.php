4b1a90d85c5e148aa8983234d2d48adf9d738ea1
pyoung4868 <pjy111@gmail.com>
2016-02-24 14:01:16 -0500

    Update Bio

--- application/views/index/fhildy.php
+++ application/views/index/fhildy.php
@@ -1,18 +1,18 @@
 <!-- Featured theatres -->
 <div class="grid_12 featuredarea">   
-	<div class="grid_12 alpha omega theatreName"><h1>Frank Hildy, General Editor</h1></div>
+	<div class="grid_12 alpha omega theatreName"><h1>Franklin J. Hildy, General Editor</h1></div>
 
     <div class="grid_3 alpha sidebar">
 		<img class="bioimg" src="../images/fhildy.jpg" alt="Frank J. Hildy" />
     </div>
     <div class="grid_9 omega maincontent">
 
-            <h3>Frank Hildy, General Editor</h3>
+            <h3>Franklin J. Hildy, General Editor</h3>
             <p>Franklin J. Hildy is Professor of Theatre and Director of the PhD Program in the School 
 of Theatre, Dance and Performance Studies at the University of Maryland, College Park. He also serves 
 as the Director of Shakespeare’s Globe (USA) Research Archive. He works as a theatre consultant 
 for historic theatre preservation and reconstruction, and is a member of the Architecture Research 
-Group for Shakespeare’s Globe in London.</p>
+Group for Shakespeare’s Globe in London. He is the founding convenor of the architecture working group for the International Federation for Theatre Research.</p>
 <p>Dr. Hildy has conducted field research on historic theatres in twenty one countries including  Austria 1989, 2002; Croatia 1986; Czech Republic 1994, 2002, 2007; 
 Denmark 1973, 2003, 2009; England 1973, 1978-79, 1983, 1986, 1988 and twice a year since then; France 1973, 1983, 1989, 2004; 
 Germany 1983, 1989, 2002, 2003; Greece 1979, 1993, 1999; Israel 2006; Italy 1974, 1983, 1994, 2006; Japan 1991; 
