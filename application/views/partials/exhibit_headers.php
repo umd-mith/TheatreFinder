73ad1d9eb3082f7758435206fc3cf9813a77801b
Ed Summers <ehs@pobox.com>
2016-10-31 15:55:34 -0400

    try using bundle

--- application/views/partials/exhibit_headers.php
+++ application/views/partials/exhibit_headers.php
@@ -1,6 +1,9 @@
 <link href="<?php echo base_url(); ?>theatres/exhibit_json" type="application/json" rel="exhibit/data" />
 
-<script src="<?php echo base_url();?>javascript/exhibit/api-2.0/exhibit-api.js"></script>
+<script src="<?php echo base_url();?>x/javascript/exhibit/api-2.0/exhibit-api.js"></script>
+
+<script src="<?php echo base_url();?>javascript/exhibit/api-2.0/exhibit-bundle.js"></script>
+
 <script src="<?php echo base_url();?>javascript/exhibit/extensions-2.0/time/time-extension.js"></script>
 <script type="text/javascript">
 function confirmDeleteTheatre(url) {
