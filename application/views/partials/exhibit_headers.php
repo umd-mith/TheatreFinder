0e597ff9545250139c77bae7cba0ac39ab05c6bd
Ed Summers <ehs@pobox.com>
2016-10-31 15:58:24 -0400

    try using bundle

--- application/views/partials/exhibit_headers.php
+++ application/views/partials/exhibit_headers.php
@@ -1,6 +1,6 @@
 <link href="<?php echo base_url(); ?>theatres/exhibit_json" type="application/json" rel="exhibit/data" />
 
-<script src="<?php echo base_url();?>x/javascript/exhibit/api-2.0/exhibit-api.js"></script>
+<script src="<?php echo base_url();?>/javascript/exhibit/api-2.0/exhibit-api.js"></script>
 
 <script src="<?php echo base_url();?>javascript/exhibit/api-2.0/exhibit-bundle.js"></script>
 
